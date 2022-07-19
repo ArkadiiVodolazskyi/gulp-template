0f580badda7a9e9ca94009c8014283deec8bd621
Arkadii Vodolazskyi <49953024+ArkadiiVodolazskyi@users.noreply.github.com>
2022-07-19 11:03:12 +0200

    Pug - basic compiling.

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,5 +1,30 @@
 import gulp from 'gulp';
 
-export const dev = () => {
+// ----- Plugins -----
+
+import pug from 'gulp-pug'; // https://www.npmjs.com/package/gulp-pug
+
+// ----- Routes -----
+
+const routes = {
+	pug: {
+		src: 'src/*.pug',
+		dest: 'build'
+	}
+}
+
+// ----- Tasks -----
+
+export const cpug = () => {
+
+	gulp.src(routes.pug.src) // https://gulpjs.com/docs/en/api/src
+	.pipe(pug())
+	.pipe(gulp.dest(routes.pug.dest)) // https://gulpjs.com/docs/en/api/dest
+
+}
+
+export const dev = async () => {
 	console.log('Dev command executed');
+
+	gulp.series([cpug]); // https://gulpjs.com/docs/en/api/series
 }
